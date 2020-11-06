7ff4d5542e2f7e42ea669e979d9c1a028d2ba736
Ortega <Ortega@10.10.214.72>
2020-11-06 01:00:34 +0000

    Kata 3 version 1

---
+++ src/kata3/HistogramDisplay.java
@@ -0,0 +1,42 @@
+package kata3;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class HistogramDisplay extends ApplicationFrame {
+    
+
+    public HistogramDisplay() {
+        super("HISTOGRAMA");
+        this.setContentPane(createPanel());
+        this.pack();
+    }
+
+    public void execute() {
+        setVisible(true);
+    }
+
+    private JPanel createPanel() {
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(400, 500));
+        return chartPanel;
+    }
+
+    private JFreeChart createChart(DefaultCategoryDataset dataSet) {
+        JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChart", "Dominios email", "Nº de emails",
+                dataSet, PlotOrientation.VERTICAL, false, false, rootPaneCheckingEnabled);
+        return chart;
+    }
+
+    private DefaultCategoryDataset createDataset() {
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.addValue(3, "cosa", "ulpgc.es");
+        return dataSet;
+    }
+}
