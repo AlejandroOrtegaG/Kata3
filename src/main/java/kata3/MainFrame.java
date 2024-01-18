e4bd29ebfe77e3cee59bceb8903ed8e77d1e13dc
AlejandroOrtegaG <alejandroog99@gmail.com>
2024-01-18 17:26:39 +0000

    Creado el mainFrame

---
+++ src/main/java/kata3/MainFrame.java
@@ -0,0 +1,26 @@
+package kata3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private HistogramDisplay histogramDisplay;
+
+    public MainFrame() throws HeadlessException {
+        this.setTitle("Histogram");
+        this.setSize(600,600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.add(createHistogramDisplay());
+    }
+
+    private Component createHistogramDisplay() {
+        JFreeChartHistogramDisplay display = new JFreeChartHistogramDisplay();
+        this.histogramDisplay = display;
+        return display;
+    }
+
+    public HistogramDisplay getHistogramDisplay(){
+        return histogramDisplay;
+    }
+}
